d66519b3b34bad592461beb6083ef7c640e05af5
moimoi22 <linhpham2003zx@gmail.com>
2025-10-10 16:08:58 +0700

    dashboard and add meal ui

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,6 +14,7 @@ android {
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        vectorDrawables.useSupportLibrary = true
     }
 
     buildTypes {
@@ -30,7 +31,6 @@ android {
         targetCompatibility = JavaVersion.VERSION_1_8
     }
 }
-
 dependencies {
 
     implementation(libs.appcompat)
@@ -39,11 +39,16 @@ dependencies {
     implementation(libs.constraintlayout)
     implementation(libs.camera.core)
     implementation(libs.camera.lifecycle)
+    implementation(libs.navigation.fragment)
+    implementation(libs.navigation.ui)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
     implementation("androidx.camera:camera-camera2:1.5.0")
     implementation("androidx.camera:camera-lifecycle:1.3.0")
     implementation("androidx.camera:camera-view:1.5.0")
+    implementation("com.github.hadibtf:SemiCircleArcProgressBar:1.1.1")
+    implementation("com.github.PhilJay:MPAndroidChart:v3.1.0")
+    implementation("com.google.android.material:material:1.12.0")
 
 }
